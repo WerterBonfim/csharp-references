207c58634f8ff39fb28548b2beeb79e5634a595e
werter bonfim <werter@hotmail.com.br>
2021-11-01 00:46:20 -0300

    Inicio

---
+++ threads/Werter.ThreadSafeLearning/Werter.ThreadSafeLearning.QueueProblems/Robot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Werter.ThreadSafeLearning.QueueProblems
+{
+    internal struct Robot
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Team { get; set; }
+        public ConsoleColor TeamColor { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}: Team: {Team}, {Name}";
+    
+    }
+    }
+}
